ca6811bd25c3b95c1a0edc548d493a23e9fe56e1
Đorđe Milanović <djordje200179@gmail.com>
2023-04-07 11:34:31 +0200

    Prediction functions minor improvements

--- spellio/predictions.go
+++ spellio/predictions.go
@@ -1,14 +1,14 @@
 package spellio
 
-func (e *Engine) PredictWord(prefix string, count int) []string {
+func (e *Engine) PredictWord(prefix string, limit int) []string {
 	allWords := e.GetWordsByPrefix(prefix)
 	words := make([]string, 0, len(allWords))
 
-	for i := 0; i < count && len(allWords) > 0; i++ {
+	for i := 0; i < limit && len(allWords) > 0; i++ {
 		var maxWord string
 		var maxOccurrence uint
 		for word, occurrences := range allWords {
-			if occurrences > maxOccurrence {
+			if occurrences > maxOccurrence || maxWord == "" {
 				maxWord = word
 				maxOccurrence = occurrences
 			}
@@ -23,13 +23,13 @@ func (e *Engine) PredictWord(prefix string, count int) []string {
 
 const allowedChangesQuotient = 3
 
-func (e *Engine) CorrectWord(rawWord string, layout KeyboardLayoutNearbyKeys, limit uint) []string {
+func (e *Engine) CorrectWord(rawWord string, layout KeyboardLayoutNearbyKeys, limit int) []string {
 	allowedChanges := (len(rawWord) + allowedChangesQuotient - 1) / allowedChangesQuotient
 
 	nearbyWords := e.GetNearbyWords(rawWord, uint(allowedChanges), layout)
 	words := make([]string, 0, len(nearbyWords))
 
-	for i := uint(0); i < limit && len(nearbyWords) > 0; i++ {
+	for i := 0; i < limit && len(nearbyWords) > 0; i++ {
 		var minWord string
 		var minChanges uint
 		for word, changes := range nearbyWords {
