9f144b8649ad38877ae7fd3c81f9a0fb0ec5898d
Đorđe Milanović <djordje200179@gmail.com>
2023-04-08 13:10:25 +0200

    More texts added

--- main.go
+++ main.go
@@ -24,6 +24,12 @@ func main() {
 	inputFromFile("books/Lord of the rings 2.txt", &engine)
 	inputFromFile("books/Lord of the rings 3.txt", &engine)
 	inputFromFile("books/Harry Potter - The Philosopher's Stone.txt", &engine)
+	inputFromFile("books/Harry Potter - The Chamber of Secrets.txt", &engine)
+	inputFromFile("books/Harry Potter - The Prisoner of Azkaban.txt", &engine)
+	inputFromFile("books/Harry Potter - The Goblet of Fire.txt", &engine)
+	inputFromFile("books/Harry Potter - The Order of the Phoenix.txt", &engine)
+	inputFromFile("books/Harry Potter - The Half Blood Prince.txt", &engine)
+	inputFromFile("books/Harry Potter - The Deathly Hallows.txt", &engine)
 
 	fmt.Println(engine.CountWords())
 }
